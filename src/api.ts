984b3281736e9e424f7541128902c2bca10955ef
NathanJAOMAMY <nathanjaomamy@gmail.com>
2025-09-04 10:40:43 +0300

    fix api

--- src/api.ts
+++ src/api.ts
@@ -2,4 +2,4 @@
 
 // export const API_BASE_URL = 'http://localhost:3001';
 // online server
-export const API_BASE_URL = 'https://promabio.com/intranetpmb';
+export const API_BASE_URL = 'https://back-intranet.onrender.com';
