739a959aab6ac6a8d76ddf732efae2560b3acd4f
NathanJAOMAMY <nathanjaomamy@gmail.com>
2025-09-20 09:28:27 +0300

    lien render

--- src/api.ts
+++ src/api.ts
@@ -2,4 +2,5 @@
 
 // export const API_BASE_URL = 'http://localhost:3001';
 // online server
-export const API_BASE_URL = 'https://env-7423207.jcloud-ver-jpe.ik-server.com';
+// export const API_BASE_URL = 'https://env-7423207.jcloud-ver-jpe.ik-server.com';
+export const API_BASE_URL = 'https://back-intranet.onrender.com';
