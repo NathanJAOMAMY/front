f2dae2a89cc14544516ce68a893648ee18a5bc7d
NathanJAOMAMY <nathanjaomamy@gmail.com>
2025-10-02 09:47:34 +0300

    lien render

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 // Local server
 
-export const API_BASE_URL = 'http://localhost:3001';
+// export const API_BASE_URL = 'http://localhost:3001';
 // online server
-// export const API_BASE_URL = 'https://back-intranet.onrender.com';
+export const API_BASE_URL = 'https://back-intranet.onrender.com';
