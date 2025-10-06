0dac1b338cf9b79e38ed74b220b91f2dce1d080c
NathanJAOMAMY <nathanjaomamy@gmail.com>
2025-10-06 17:56:26 +0300

    maj api

--- src/api.ts
+++ src/api.ts
@@ -2,4 +2,5 @@
 
 // export const API_BASE_URL = 'http://localhost:3001';
 // online server
-export const API_BASE_URL = 'https://back-intranet.onrender.com';
+// export const API_BASE_URL = 'https://back-intranet.onrender.com';
+export const API_BASE_URL = 'https://env-2523343.jcloud-ver-jpe.ik-server.com/';
